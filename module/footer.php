04a1d25d1d3d99bb232b06c5a5ea4a044a9cb290
Tan Nguyen <tannguyen512001@gmail.com>
2022-08-06 12:19:52 +0700

    Push

---
+++ module/footer.php
@@ -0,0 +1,18 @@
+<div class="footer">
+    <div class="top">
+        <span>NHÓM ALT</span>
+    </div>
+    <div class="left">
+        <h4>DANANG ARCHITECTURE UNIVERTY</h4>
+        <p>Môn: Lập trình web</p>
+        <p>gv: Nguyễn Thị Xuân Thủy</p>
+    </div>
+    <div class="right">
+        <h4>Thành Viên</h4>
+        <ul>
+            <li>Võ Hà An</li>
+            <li>Nguyễn Vũ Duy Luân</li>
+            <li>Nguyễn Duy Tân</li>
+        </ul>
+    </div>
+</div>
